3b6c159598edfb2d5a2e0d53e3dca22e82130155
Ted Romer <tromer@gmail.com>
2026-07-10 21:34:57 -0700

    wip for posting raw data to 3 sheets in https://thr.s.gy/KRZMyH

--- packages/apps-script-portfolio-tracker/src/app.ts
+++ packages/apps-script-portfolio-tracker/src/app.ts
@@ -1,9 +1,25 @@
 // Make changes in https://github.com/righteffort/empower-poster/packages/apps-script-portfolio-tracker/src/
-
-import type { PostPayload } from "@righteffort/empower-poster-types";
+import type {
+  PostPayload,
+  PostResponse,
+  HoldingEntry,
+  Classification,
+  Classifications,
+  Account,
+} from "@righteffort/empower-poster-types";
 
 function fail(message = "Internal error"): never {
   throw new Error(message);
+}
+
+/**
+ * Add a leading apostrophe to purely numeric account names and types.
+ */
+function formatAsString(accountName: string): string {
+  if (accountName.search(/^[0-9]+$/) === 0) {
+    return `'${accountName}`;
+  }
+  return accountName;
 }
 
 function getCacheVersion() {
@@ -72,6 +88,201 @@ export function EMPOWER_VALUE(
   return values[rowNumber - 1] ?? "";
 }
 
+const EMPOWER_ACCOUNTS_SHEET_NAME = "Empower Accounts";
+const EMPOWER_ASSETS_SHEET_NAME = "Empower Assets";
+const EMPOWER_HOLDINGS_SHEET_NAME = "Empower Holdings";
+
+class AssetAllocationUpater {
+  private readonly holdingsArray: HoldingEntry[];
+  private readonly classifications: Record<string, Classification[]>;
+  private readonly accountMap: Map<number, Account>;
+  private readonly spreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet;
+  private readonly empowerAccountsSheet: GoogleAppsScript.Spreadsheet.Sheet;
+  private readonly empowerAssetsSheet: GoogleAppsScript.Spreadsheet.Sheet;
+  private readonly empowerHoldingsSheet: GoogleAppsScript.Spreadsheet.Sheet;
+
+  constructor(
+    holdingsArray: HoldingEntry[],
+    classifications: Classifications,
+    accounts: Account[],
+  ) {
+    const allAccounts = new Map(accounts.map((a) => [a.id, a]));
+    const accountIds = [...new Set(holdingsArray.map((h) => h.userAccountId))];
+    this.accountMap = new Map(
+      accountIds.map((id) => [
+        id,
+        allAccounts.get(id) || fail("Unreachable code"),
+      ]),
+    );
+    this.holdingsArray = holdingsArray;
+    this.classifications = classifications;
+    this.spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    this.empowerAccountsSheet =
+      this.spreadsheet.getSheetByName(EMPOWER_ACCOUNTS_SHEET_NAME) ||
+      fail(`'${EMPOWER_ACCOUNTS_SHEET_NAME}' sheet missing`);
+    this.empowerAssetsSheet =
+      this.spreadsheet.getSheetByName(EMPOWER_ASSETS_SHEET_NAME) ||
+      fail(`'${EMPOWER_ASSETS_SHEET_NAME}' sheet missing`);
+    this.empowerHoldingsSheet =
+      this.spreadsheet.getSheetByName(EMPOWER_HOLDINGS_SHEET_NAME) ||
+      fail(`'${EMPOWER_HOLDINGS_SHEET_NAME}' sheet missing`);
+  }
+
+  updateSpreadsheet() {
+    this.updateEmpowerAccountsSheet();
+    this.updateEmpowerAssetsSheet();
+    this.updateEmpowerHoldingsSheet();
+  }
+
+  private updateEmpowerAccountsSheet() {
+    const sheet = this.empowerAccountsSheet;
+    sheet.clearContents();
+    this.updateInstitutions(0);
+    this.updateAccounts(sheet.getLastColumn() + 2);
+  }
+
+  private updateInstitutions(column: number) {
+    const institutions = [
+      ...new Set([...this.accountMap.values()].map((a) => a.firmName)),
+    ].sort();
+    const sheet = this.empowerAccountsSheet;
+    const institutionsRange = sheet.getRange(
+      1,
+      column,
+      institutions.length + 1,
+      1,
+    );
+    institutionsRange.setValues(
+      ["Institutions:Name", ...institutions].map((c) => [formatAsString(c)]),
+    );
+  }
+
+  private updateAccounts(column: number) {
+    // TODO: We also have to massage the contents of the Accounts table in Account Setup!
+    const accounts: [string, string][] = [...this.accountMap.values()]
+      .sort((a: Account, b: Account) => a.name.localeCompare(b.name))
+      .map((a) => [formatAsString(a.name), formatAsString(a.firmName)]);
+    const ACCOUNTS_TABLE_NAME = "Accounts";
+    const headers = ["Name", "Institution"].map(
+      (c) => `${ACCOUNTS_TABLE_NAME}:${c}`,
+    );
+    const sheet = this.empowerAccountsSheet;
+    const accountsRange = sheet.getRange(1, column, accounts.length + 1, 2);
+    accountsRange.setValues([headers, ...accounts]);
+  }
+
+  private updateEmpowerHoldingsSheet() {
+    const holdingRows = this.holdingsArray
+      .map((h) => ({
+        accountName: this.accountMap.get(h.userAccountId)?.name ?? "Unknown",
+        ticker: h.ticker,
+        quantity: h.quantity,
+      }))
+      .sort(
+        (a, b) =>
+          a.accountName.localeCompare(b.accountName) ||
+          a.ticker.localeCompare(b.ticker),
+      )
+      .map((r) => [r.accountName, r.ticker, r.quantity]);
+    const HOLDINGS_TABLE_NAME = "Holdings";
+    const sheet = this.empowerHoldingsSheet;
+    sheet.clearContents();
+    const headers = ["Account", "Asset", "Shares"].map(
+      (c) => `${HOLDINGS_TABLE_NAME}:${c}`,
+    );
+    sheet
+      .getRange(1, 1, holdingRows.length + 1, 3)
+      .setValues([headers, ...holdingRows]);
+  }
+
+  private adjustClasses(classes: [string, string]): [string, string] {
+    const [parent, child] = classes;
+    if (!child) {
+      return [parent, parent];
+    }
+    const m = parent.match(/^(Intl|U.S.) /);
+    if (m) {
+      return [parent, `${m[1]} ${child}`];
+    }
+    return [parent, child];
+  }
+
+  private updateEmpowerAssetsSheet() {
+    const flatAssets = Object.entries(this.classifications).flatMap(
+      ([ticker, v]) =>
+        v.map((c) => ({
+          ticker,
+          classes: this.adjustClasses(c.classes),
+          fraction: c.fraction,
+        })),
+    );
+    // const classCategories = [
+    //   ...new Set(flatAssets.map((a) => a.classes[0] ?? "")),
+    // ];
+    const assetClasses = Array.from(
+      new Set(flatAssets.map((a) => a.classes.join("\0"))),
+    )
+      .map((v) => v.split("\0") as [string, string])
+      .sort(
+        (a: [string, string], b: [string, string]) =>
+          a[0].localeCompare(b[0]) || a[1].localeCompare(b[1]),
+      );
+    const sheet = this.empowerAssetsSheet;
+    sheet.clearContents();
+    this.updateAssets(flatAssets, 0);
+    this.updateAssetClasses(assetClasses, sheet.getLastColumn() + 2);
+    // this.updateClassCategories(classCategories, sheet.getLastColumn() + 2);
+  }
+  updateAssets(
+    flatAssets: {
+      ticker: string;
+      classes: [string, string];
+      fraction: number;
+    }[],
+    column: number,
+  ) {
+    const holdings = new Map<string, HoldingEntry>(
+      this.holdingsArray.map((h) => [h.ticker, h]),
+    );
+    const assetRows = flatAssets
+      .sort((a, b) => a.ticker.localeCompare(b.ticker))
+      .map((a) => {
+        const h = holdings.get(a.ticker);
+        // TODO: Check that this interoperates with our "column formulas" for name and price
+        return [
+          a.ticker,
+          h?.cusip ?? "",
+          h?.cusip ? "" : a.ticker,
+          a.classes[1],
+          a.fraction,
+          h?.cusip ? "" : h?.price,
+        ];
+      });
+    const headers = [
+      "Ticker",
+      "Cusip",
+      "Name",
+      "Class",
+      "Class Pct",
+      "Price",
+    ].map((c) => `Assets:${c}`);
+    this.empowerAssetsSheet
+      .getRange(1, column, assetRows.length + 1, headers.length)
+      .setValues([headers, ...assetRows]);
+  }
+
+  private updateAssetClasses(assetClasses: string[][], column: number) {
+    const headers = ["Name", "Category"].map((c) => `Asset Classes:${c}`);
+    this.empowerAssetsSheet
+      .getRange(1, column, assetClasses.length, headers.length)
+      .setValues([headers, ...assetClasses.map((cs) => [cs[1], cs[0]])]);
+  }
+
+  // private updateClassCategories(incomingClassCategories: string[], column: number) {
+  //   // TODO: write nothing at all (current), or preserve existing mappings.
+  // }
+}
+
 export function doPost(event: GoogleAppsScript.Events.DoPost) {
   try {
     const {
@@ -84,6 +295,40 @@ export function doPost(event: GoogleAppsScript.Events.DoPost) {
     Logger.log(`${holdings.length} holdings`);
     Logger.log(`${Object.entries(classifications).length} classifications`);
     Logger.log(`${accounts.length} accounts`);
+    const supported = { major: 0, minor: 6 };
+    if (major !== supported.major || minor < supported.minor) {
+      throw new Error(
+        `data version ${major}.${minor} not supported, expected at least ${supported.major}.${supported.minor}`,
+      );
+    }
+
+    let lock: GoogleAppsScript.Lock.Lock | undefined;
+    try {
+      lock = LockService.getScriptLock();
+      lock.waitLock(30_000);
+      new AssetAllocationUpater(
+        holdings,
+        classifications,
+        accounts,
+      ).updateSpreadsheet();
+    } finally {
+      lock?.releaseLock();
+    }
+    const responseBody: PostResponse = {
+      success: true,
+      message: "Data received",
+    };
+    return ContentService.createTextOutput(
+      JSON.stringify(responseBody),
+    ).setMimeType(ContentService.MimeType.JSON);
+  } catch (e) {
+    const responseBody: PostResponse = {
+      success: false,
+      error: e instanceof Error ? e.message : String(e),
+    };
+    return ContentService.createTextOutput(
+      JSON.stringify(responseBody),
+    ).setMimeType(ContentService.MimeType.JSON);
   } finally {
     invalidateCache();
   }
